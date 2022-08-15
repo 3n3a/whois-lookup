7137f4c1a51ff44e0cec394692eb9fe9d51c127c
3n3a <gh@3n3a.ch>
2022-08-15 08:31:46 +0000

    first integration dayns

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,18 +1,18 @@
 import { h, Fragment } from 'preact';
 import { useState } from 'preact/hooks';
 import './Counter.css';
+import * as dayjs from 'dayjs'
+import 'dayjs/locale/de-ch'
+dayjs.locale('de-ch')
 
 export default function Counter({ children }) {
-	const [count, setCount] = useState(0);
-	const add = () => setCount((i) => i + 1);
-	const subtract = () => setCount((i) => i - 1);
+	//const [time, setTime] = useState(0);
+	const time = dayjs().diff(dayjs().add(8, 'hours'), 'hours', true)
 
 	return (
 		<>
 			<div class="counter">
-				<button onClick={subtract}>-</button>
-				<pre>{count}</pre>
-				<button onClick={add}>+</button>
+				<pre>{time} hours</pre>
 			</div>
 			<div class="counter-message">{children}</div>
 		</>
