a10b35260606ff16ee9f740c569dc7fe88807de8
3n3a <46775561+3n3a@users.noreply.github.com>
2022-10-01 16:28:16 +0200

    Update [id].js

--- functions/api/lookup/[id].js
+++ functions/api/lookup/[id].js
@@ -2,20 +2,14 @@ import {redirectList} from '../../rdap-list';
 import {parse_host} from '../../extract-tld';
 
 function getRdapServer(domainName) {
-  let tld = "ch";
-  /*try {
-   tld = parse_host(domainName).tld;
-  } catch (e) {
-   return [false, {line: "getRdapSrrver", message: e.message, stack: e.hasOwnProperty("stack") ? e.stack : "", }];
-  }*/
-  return [true, redirectList[tld]];
+  let tld_res = parse_host(domainName);
+  return [true, redirectList[tld_res.tld]];
 }
 
 export async function onRequestGet({ params }) {
     let result;
     try {
       const [success, rdapUrl] = getRdapServer(params.id);
-      //if (!success) return new Response(JSON.stringify(rdapUrl, null, 2), { status: 500 });
       const res = await fetch(`${rdapUrl}domain/${params.id}`);
       const data = await res.json();
       const info = JSON.stringify(data, null, 2);
