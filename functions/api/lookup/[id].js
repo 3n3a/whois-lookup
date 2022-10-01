2c7604cc18479e2fe397bdb1fef19cc67601e0ef
3n3a <info@3n3a.ch>
2022-10-01 13:52:18 +0200

    add all rdap server list

--- functions/api/lookup/[id].js
+++ functions/api/lookup/[id].js
@@ -1,5 +1,14 @@
+import {redirectList} from '../../rdap-list';
+import {parse_host} from '../../extract-tld';
+
+function getRdapServer(domainName) {
+  const tld = parse_host(domainName).tld;
+  return redirectList[tld];
+}
+
 export async function onRequestGet({ params }) {
-    const res = await fetch(`https://rdap.nic.ch/domain/${params.id}`);
+    const rdapUrl = getRdapServer(params.id);
+    const res = await fetch(`https://${rdapUrl}/domain/${params.id}`);
     const data = await res.json();
     const info = JSON.stringify(data, null, 2);
     return new Response(info);
---
+++ functions/extract-tld.js
@@ -0,0 +1,42 @@
+
+"use strict";
+var tlds = null;
+
+var parse_host = function(host, options) {
+  let allowPrivateTLD = options && options.allowPrivateTLD || false;
+  let allowUnknownTLD = options && options.allowUnknownTLD || false;
+
+  if(!tlds) {
+    tlds = require('./effective_tld_names.json');
+    tlds.combined = Object.assign({}, tlds.icann, tlds.private);
+  }
+
+  var parts = host.split(".");
+  var stack = "", tld_level = -1;
+
+  var roots = allowPrivateTLD ? tlds.combined : tlds.icann;
+
+  for(var i = parts.length - 1, part; i >= 0; i--) {
+    part = parts[i];
+    stack = stack ? part + "." + stack : part;
+    if(roots[stack])
+      tld_level = roots[stack];
+  }
+
+  if(tld_level == -1 && allowUnknownTLD)
+    tld_level = 1;
+
+  if(parts.length <= tld_level || tld_level == -1)
+    throw new Error("Invalid TLD " + JSON.stringify({parts, tld_level, allowUnknownTLD}));
+
+  return  {
+    tld     : parts.slice(-tld_level).join('.'),
+    domain  : parts.slice(-tld_level - 1).join('.'),
+    sub     : parts.slice(0, (-tld_level - 1)).join('.'),
+  };
+};
+
+
+
+
+module.exports.parse_host = parse_host;
